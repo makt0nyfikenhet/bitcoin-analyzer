9acff81f373a4e4fbd6f37732eed3c2bf22b832e
gabriela-oss <solisch.gabriela@gmail.com>
2022-10-28 09:53:37 -0600

    List items

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 // Libs
 import { BaseSyntheticEvent, FC, ReactNode, useState } from "react";
 // Styles
-import styles from "./App.module.css";
-
+import styles from "./App.module.css"; 
 interface Props {
   children?: ReactNode;
+}
+interface Product{
+  name: string;
+  price: number;
+}
+
+const ListItem: FC<Product> = (props) =>{
+  return(  
+    <>
+      <li>
+        {props.name}
+      </li>
+      <li>
+        {props.price}
+      </li>
+    </> 
+  )
 }
 
 const Container: FC<Props> = ({ children }) => (
@@ -14,26 +30,21 @@ const ContainerOverlay: FC<Props> = ({ children }) => (
   <div className={styles.containerOverlay}>{children}</div>
 );
 
-interface Product {
-  name: string;
-  price: number;
-}
-
 function App() {
-  const [productList, setProductList] = useState<Product[]>([]);
+  const [productList, setProductList] = useState<Props[]>([]);
   const [currentProduct, setCurrentProduct] = useState<Product>({
     name: "",
     price: 0,
   });
-
+  
   console.log(currentProduct);
   console.log(productList);
-
+  
   const handleOnChange = (e: BaseSyntheticEvent) =>
-    setCurrentProduct((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-
+  setCurrentProduct((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  
   const handleOnAddToList = () =>
-    setProductList((prev) => [...prev, currentProduct]);
+  setProductList((prev) => [...prev, currentProduct]);
 
   return (
     <Container>
@@ -53,9 +64,45 @@ function App() {
           onChange={handleOnChange}
         />
         <button onClick={handleOnAddToList}>Add to cart</button>
+        <ListItem name={currentProduct.name} price={currentProduct.price}/>
       </ContainerOverlay>
     </Container>
   );
 }
 
-export default App;
+export default App; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/*    setCurrentProduct((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
+Esto es para actualizar dinámicamente la propiedad del objeto (cuando el nombre de la propiedad es desconocido por adelantado pero en tiempo de ejecución). De esta manera, podría tener múltiples entradas de React con una propiedad de nombre diferente y usando el mismo controlador onChange para actualizar parte del estado.*/}
