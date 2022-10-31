e8657227832f2c95f9cd8f3b62d2ed50f372d5e4
makt0nyfikenhet <rodriguez.juan0293@gmail.com>
2022-10-30 19:07:44 -0600

    fix JSX sintax

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
 // Libs
 import { BaseSyntheticEvent, FC, ReactNode, useState } from "react";
 // Styles
-import styles from "./App.module.css"; 
+import styles from "./App.module.css";
 interface Props {
   children?: ReactNode;
 }
-interface Product{
+interface Product {
   name: string;
   price: number;
 }
-
-const ListItem: FC<Product> = (props) =>{
-  // console.log(props, "hola")
-  const _data = Object.values(props)
-  console.log(_data, "hola")
-  return(
-    <>
-      {
-        _data.map((li, index)=>{  
-        {console.log(li + " " + "prueba")}
-        <ol key={index}>    
-          <li>
-            {li}
-          </li>
-       </ol>
-    })
-      }
-    </>
-  )
+interface ProductList {
+  productList: Product[];
 }
+
+const ProductList: FC<ProductList> = (props) => {
+  // console.log(props, "hola")
+  const _data = Object.values(props);
+  console.log(_data, "hola");
+  return (
+    <>
+      {_data.map((li, index) => {
+        {
+          console.log(li + " " + "prueba");
+        }
+        <ol key={index}>
+          <li>{li}</li>
+        </ol>;
+      })}
+    </>
+  );
+};
+
+const ListItem: FC<Product> = (props) => {
+  // console.log(props, "hola")
+  const _data = Object.values(props);
+  console.log(_data, "hola");
+
+  // La razón por la que no le renderiza el código <ol>...</ol> es porque el .map, aunque esté dentro de un return, el map a su vez lo que hace es
+  // ejecutar otra función dentro de él, por lo que la función del .map también debe tener su propio return, el return del map le retorna al return del
+  // componente ListItem, y este segundo le retorna a React el código HTML y React lo retorna al DOM
+  return (
+    <>
+      {_data.map((li, index) => {
+        {
+          console.log(li + " " + "prueba");
+        }
+        return (
+          <ol key={index} style={{ color: "white" }}>
+            <li>{li}</li>
+          </ol>
+        );
+      })}
+    </>
+  );
+};
 
 const Container: FC<Props> = ({ children }) => (
   <div className={styles.container}>{children}</div>
@@ -43,15 +68,17 @@ function App() {
     name: "",
     price: 0,
   });
-  
+
   console.log(currentProduct);
   console.log(productList);
-  
-  const handleOnChange = (e: BaseSyntheticEvent) => //to can write in the input field
-  setCurrentProduct((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  
+
+  const handleOnChange = (
+    e: BaseSyntheticEvent //to can write in the input field
+  ) =>
+    setCurrentProduct((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
   const handleOnAddToList = () =>
-  setProductList((prev) => [...prev, currentProduct]); //add new product and save it in a new array 
+    setProductList((prev) => [...prev, currentProduct]); //add new product and save it in a new array
 
   return (
     <Container>
@@ -70,53 +97,21 @@ function App() {
           placeholder="Precio del producto"
           onChange={handleOnChange}
         />
-        <button onClick={handleOnAddToList} value="submit">Add to cart</button>
+        <button onClick={handleOnAddToList} value="submit">
+          Add to cart
+        </button>
         <ul>
-          <ListItem 
-            name={currentProduct.name}
-            price={currentProduct.price}
-          />
+          <ListItem name={currentProduct.name} price={currentProduct.price} />
         </ul>
-        
-        
       </ContainerOverlay>
     </Container>
   );
 }
 
-export default App; 
+export default App;
 
+{
+  /*    setCurrentProduct((prev) => ({ ...prev, [e.target.name]: e.target.value }));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/*    setCurrentProduct((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-
-Esto es para actualizar dinámicamente la propiedad del objeto (cuando el nombre de la propiedad es desconocido por adelantado pero en tiempo de ejecución). De esta manera, podría tener múltiples entradas de React con una propiedad de nombre diferente y usando el mismo controlador onChange para actualizar parte del estado.*/}
+Esto es para actualizar dinámicamente la propiedad del objeto (cuando el nombre de la propiedad es desconocido por adelantado pero en tiempo de ejecución). De esta manera, podría tener múltiples entradas de React con una propiedad de nombre diferente y usando el mismo controlador onChange para actualizar parte del estado.*/
+}
