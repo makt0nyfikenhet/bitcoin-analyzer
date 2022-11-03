f05c28e068dae431ef87d940229ca5d3912f81ea
makt0nyfikenhet <rodriguez.juan0293@gmail.com>
2022-11-03 12:15:04 -0600

    fix onSetProductList function type

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,37 +1,40 @@
+// Libs
 import { useState } from "react";
-import { Form1 } from "./components/Form1";
+// Types
+import { Product } from "@/types";
+// Components
+import { FormInputs } from "./components/FormInputs";
 import { ProductList } from "./components/ProductList";
 
-export interface Product {
-	name: string;
-	price?: string;
-	id?: number | null;
-}
-const inicialValue: Product[] = [
-	{
-		name: "string",
-		price: "string",
-		id: 5,
-	},
+const initialValue: Product[] = [
+  {
+    name: "string",
+    price: "string",
+    id: 5,
+  },
 ];
 const ProductForm = () => {
-	const [dataToEdit, setDataToEdit] = useState(null);
-	const [productList, setProductList] = useState(inicialValue);
-	return (
-		<>
-			<Form1
-				setProductList={setProductList}
-				placeHolder1={"hola"}
-				placeHolder2={"ke ace"}
-			>
-				<ProductList
-					productList={productList}
-					dataToEdit={dataToEdit}
-					setDataToEdit={setDataToEdit}
-				/>
-			</Form1>
-		</>
-	);
+  const [dataToEdit, setDataToEdit] = useState(null);
+  const [productList, setProductList] = useState(initialValue);
+
+  const onSetProductList = (currentProduct: Product) =>
+    setProductList((prev) => [...prev, currentProduct]);
+
+  return (
+    <>
+      <FormInputs
+        setProductList={onSetProductList}
+        placeHolderName={"hola"}
+        placeHolderPrice={"ke ace"}
+      >
+        <ProductList
+          productList={productList}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+      </FormInputs>
+    </>
+  );
 };
 
 export default ProductForm;
--- src/components/ProductForm/components/ProductList/ProductList.tsx
+++ src/components/ProductForm/components/ProductList/ProductList.tsx
@@ -1,68 +1,65 @@
-import { useState } from "react";
-import { BaseSyntheticEvent, FC } from "react";
-import { Product } from "../../ProductForm";
-
-import styles from "./ProductList.module.css";
+// Libs
+import { BaseSyntheticEvent, FC, useState } from "react";
+// Types
+import { Product } from "@/types";
 
 interface ProductList {
-	productList: Product[];
-	dataToEdit: any;
-	setDataToEdit: Function;
+  productList: Product[];
+  dataToEdit: any;
+  setDataToEdit: Function;
 }
 
 interface ProductItem {
-	product: Product;
+  product: Product;
 }
 const inicialValue = {
-	handdleOnClickEdit: false,
-	otraVariable2: "false",
+  handdleOnClickEdit: false,
+  otraVariable2: "false",
 };
 
 const ProductItem: FC<ProductItem> = ({ product }) => {
-	const [stateHanddle, setStateHanddle] = useState(inicialValue);
+  const [stateHanddle, setStateHanddle] = useState(inicialValue);
 
-	const handdleOnClickEdit = (
-		e: BaseSyntheticEvent,
-		otrosParametros: string
-	) => {
-		setStateHanddle((current) => ({
-			...current,
-			handdleOnClickEdit: !current.handdleOnClickEdit,
-		}));
-	};
+  const handdleOnClickEdit = (
+    e: BaseSyntheticEvent,
+    otrosParametros: string
+  ) => {
+    setStateHanddle((current) => ({
+      ...current,
+      handdleOnClickEdit: !current.handdleOnClickEdit,
+    }));
+  };
 
-	const otrosParametros = "Ejemplo de param";
+  const otrosParametros = "Ejemplo de param";
 
-	return (
-		<>
-			<li style={{ color: "white" }}>
-				<b>Product name: </b>
-				{product.name + " "}
-				<b>Product price: </b>
-				{product.price + " "}
-				<button onClick={(e) => handdleOnClickEdit(e, otrosParametros)}>
-					{stateHanddle.handdleOnClickEdit === false ? "Editar" : "No Editar"}
-				</button>
-			</li>
-			{stateHanddle.handdleOnClickEdit && (
-				<div className={styles.formulario2}>Form1</div>
-			)}
-			{console.log(stateHanddle)}
-		</>
-	);
+  return (
+    <>
+      <li style={{ color: "white" }}>
+        <b>Product name: </b>
+        {product.name + " "}
+        <b>Product price: </b>
+        {product.price + " "}
+        <button onClick={(e) => handdleOnClickEdit(e, otrosParametros)}>
+          {stateHanddle.handdleOnClickEdit === false ? "Editar" : "No Editar"}
+        </button>
+      </li>
+      {stateHanddle.handdleOnClickEdit && <div>Form1</div>}
+      {console.log(stateHanddle)}
+    </>
+  );
 };
 
 const ProductList: FC<ProductList> = (props) => {
-	return (
-		<>
-			<ul>
-				{props.productList.map((product, index) => (
-					<ProductItem key={index.toString()} product={product} />
-				))}
-			</ul>
-			<div className={styles.formulario2}>Formulario</div>
-		</>
-	);
+  return (
+    <>
+      <ul>
+        {props.productList.map((product, index) => (
+          <ProductItem key={index.toString()} product={product} />
+        ))}
+      </ul>
+      <div>Formulario</div>
+    </>
+  );
 };
 
 export default ProductList;
